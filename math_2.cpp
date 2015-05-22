c429210252c268d0a3dd558cd455a7934a55a0a6
Helena <hvictoretti@gmail.com>
2015-05-22 20:40:33 -0300

    Arrumou o código

--- math_2.cpp
+++ math_2.cpp
@@ -14,11 +14,9 @@ void factorial(mpf_t result, unsigned long x) {
 
 	if (x == 0) mpf_set_d(result, 1.0);
 
-	//for (aux; aux > 0; aux--) {
 	while( mpf_cmp_ui(aux, 0) > 0 ) {
 		mpf_mul(result, result, aux);
 		mpf_sub_ui(aux, aux, 1);
-		//result = result * aux;
 	}
 }
 
@@ -28,10 +26,8 @@ void power(mpf_t result, mpf_t b, double e) {
 	int i;
 	mpf_init(result);
 	mpf_set_d(result, 1.0);	
-	//double result = 1;
 
 	for (i = 0; i < e; i++) {
-		//result = result * b;
 		mpf_mul(result, result, b);
 	}
 }
